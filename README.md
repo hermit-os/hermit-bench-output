c7ee0610b8460820b4077d0eb0f5d3ec79e13d17
CarlWachter <carlwachter@web.de>
2024-06-21 13:51:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hermit-bench-output
+
+This crate exists to provide an interface for [hermit-bench](https://github.com/CarlWachter/hermit-bench/), which is a benchmarking tool for [hermit-os](https://github.com/hermit-os/).
+
+## Usage
+
+After specifying a command to run a file in `hermit-bench`, the function `log_benchmark_data` shall be used to provide the expected output format.
---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+/// Log benchmark data to stdout, so it can be captured by the benchmarking framework hermit-bench.
+/// 
+/// * `benchmark_name` - The name under which the benchmark will be stored.
+/// * `unit` - The unit of the benchmark value.
+/// * `value` - The benchmark value.
+pub fn log_benchmark_data(benchmark_name: &str, unit: &str, value: f64) {
+    println!(
+        "/*BENCHMARK OUTPUT*/\nname: {}\nunit: {}\nvalue: {}\n/*BENCHMARK OUTPUT END*/",
+        benchmark_name, unit, value
+    );
+}
