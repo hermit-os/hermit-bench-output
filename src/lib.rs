fea8312339f531a011b39aad5343d4af26f16868
CarlWachter <carlwachter@web.de>
2024-06-27 09:50:57 +0200

    refactor: Add log_benchmark_data_with_group function for capturing benchmark data with plot group

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,20 @@
 /// * `value` - The benchmark value.
 pub fn log_benchmark_data(benchmark_name: &str, unit: &str, value: f64) {
     println!(
-        "/*BENCHMARK OUTPUT*/\nname: {}\nunit: {}\nvalue: {}\n/*BENCHMARK OUTPUT END*/",
+        "/*BENCHMARK OUTPUT*/\nname: {}\nunit: {}\nvalue: {}\nplot_group: \n/*BENCHMARK OUTPUT END*/",
         benchmark_name, unit, value
+    );
+}
+
+/// Log benchmark data to stdout, so it can be captured by the benchmarking framework hermit-bench.
+///
+/// * `benchmark_name` - The name under which the benchmark will be stored.
+/// * `unit` - The unit of the benchmark value.
+/// * `value` - The benchmark value.
+/// * `plot_group` - The plot group in which the benchmark will be plotted.
+pub fn log_benchmark_data_with_group(benchmark_name: &str, unit: &str, value: f64, plot_group: &str) {
+    println!(
+        "/*BENCHMARK OUTPUT*/\nname: {}\nunit: {}\nvalue: {}\nplot_group: {}\n/*BENCHMARK OUTPUT END*/",
+        benchmark_name, unit, value, plot_group
     );
 }
